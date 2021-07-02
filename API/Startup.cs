a8ae716a89bcf146acc321633aa08185f1dca5db
me.saif <me.shaikhsaif@gmail.com>
2021-07-01 19:14:45 -0700

    Section Two Completed

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,17 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
+    //         services.AddCors(options =>
+    // {
+    //     options.AddPolicy(
+    //         name: "AllowOrigin",
+    //         builder =>{
+    //             builder.AllowAnyOrigin()
+    //                     .AllowAnyMethod()
+    //                     .AllowAnyHeader();
+    //         });
+    // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +62,12 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .SetIsOriginAllowed((host) => true)
+                .WithOrigins("https://localhost:4200/"));
+           // app.UseCors("AllowOrigin");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
